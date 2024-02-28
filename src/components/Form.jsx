69173eb42e426813b4c1ee82a1485fef81877c46
Huseyn Ismayilov <131815459+Huseyn-Ismayilov@users.noreply.github.com>
2024-02-28 16:20:35 +0400

    mobile responsive

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import { Label } from "./ui/Label"
 import { Button } from "./ui/Button"
 export const Form = () => {
     return (
-        <div className="">
+        <>
             <div className="mb-3">
                 <Label>Your Name</Label>
                 <Input placeholder="Johnson Doe" />
@@ -17,8 +17,8 @@ export const Form = () => {
                 <Input type="password" placeholder="***************" />
             </div>
             <div className="mb-3">
-                <Button className="bg-black text-white w-full">GET STARTED</Button>
+                <Button className="bg-black text-white w-full hover:bg-gray-800">GET STARTED</Button>
             </div>
-        </div>
+        </>
     )
 }
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,5 +1,5 @@
 export const Button = (props) => {
   return (
-    <button className={`${props.className} py-4 uppercase text-xs font-semibold rounded-lg text-center`}>{props.children}</button>
+    <button className={`${props.className} py-5 uppercase text-xs font-semibold rounded-lg text-center`}>{props.children}</button>
   )
 }
